cbfb04dab7596468b8c6ef7aae91496de9643d48
DESKTOP-SM4SNDC\Hamed <Movahedian.h@gmail.com>
2018-08-18 07:59:17 +0430

    no message

---
+++ Assets/JumpTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpTest : MonoBehaviour
+{
+    public Vector3 Force=new Vector3(0,100,0);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var ballPhysic = other.gameObject.GetComponent<BallPhysic>();
+        if(ballPhysic!=null)
+            ballPhysic.AddForce(Force);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
